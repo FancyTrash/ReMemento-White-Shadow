db86798edd73e4b439796d4afc9966c623d6a33f
FancyTrash <fancytrash786585@gmail.com>
2024-12-16 16:52:09 -0600

    Create .java

---
+++ .java
@@ -0,0 +1,19 @@
+const searchButton = document.querySelector('.search-button');
+const searchBox = document.querySelector('.search-box'); 
+const closeButton = document.querySelector('.close-button');
+const dots = document.querySelector('.dots');
+
+searchButton.addEventListener('click', () => {
+  searchButton.style.display = 'none'; 
+  searchBox.style.display = 'inline-block';
+});
+
+closeButton.addEventListener('click', () => {
+  searchBox.style.display = 'none';
+  searchButton.style.display = 'inline-block'; 
+});
+
+dots.addEventListener('click', () => {
+  // Your dropdown menu code here
+  alert("Dropdown menu will be added here!");
+});
