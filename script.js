58a1a8a5f0a9b965b2ef2eba8bf28fd20f66569a
FancyTrash <fancytrash786585@gmail.com>
2024-12-17 14:43:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const searchBox = document.querySelector('.search-box');
 const closeButton = document.querySelector('.close-button');
 const dots = document.querySelector('.dots');
 
+// Show Search Box
 searchButton.addEventListener('click', () => {
   searchButton.style.display = 'none';
-  searchBox.style.display = 'inline-block';
+  searchBox.classList.add('show');
 });
 
+// Hide Search Box
 closeButton.addEventListener('click', () => {
-  searchBox.style.display = 'none';
+  searchBox.classList.remove('show');
   searchButton.style.display = 'inline-block';
 });
 
+// Dropdown Menu Placeholder
 dots.addEventListener('click', () => {
-  // Your dropdown menu code here
   alert("Dropdown menu will be added here!");
 });
