d5408bda4b3cc3e53948543416cd2558c3df5901
FancyTrash <fancytrash786585@gmail.com>
2024-12-17 15:17:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchButton = document.getElementById("searchButton");
 const searchBox = document.getElementById("searchBox");
 const closeButton = document.getElementById("closeButton");
+const dots = document.querySelector(".dots"); // Get the dots element
 
 // Show the search box and hide the search button
 searchButton.addEventListener("click", () => {
@@ -12,4 +13,10 @@ searchButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   searchBox.style.display = "none"; // Hide search box
   searchButton.style.display = "inline-block"; // Show search button
+});
+
+// Add functionality for the dots
+dots.addEventListener("click", () => {
+  // For now, display a simple alert or you can add dropdown functionality
+  alert("Dropdown menu will be added here!");
 });
