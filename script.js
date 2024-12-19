da65bff038fb1dfba13464207d3414a0021cf0a7
FancyTrash <fancytrash786585@gmail.com>
2024-12-18 23:16:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+// Search box toggle
 const searchButton = document.getElementById("searchButton");
 const searchBox = document.getElementById("searchBox");
 const closeButton = document.getElementById("closeButton");
 
-// Search box toggle
 searchButton.addEventListener("click", () => {
-    searchBox.style.display = "flex"; 
-    searchButton.style.display = "none";
+  searchBox.style.display = "flex";
+  searchButton.style.display = "none";
 });
 
 closeButton.addEventListener("click", () => {
-    searchBox.style.display = "none";
-    searchButton.style.display = "inline-block"; 
+  searchBox.style.display = "none";
+  searchButton.style.display = "inline-block";
 });
 
 // Sidebar toggle
@@ -19,9 +19,9 @@ const sidebarToggle = document.getElementById("sidebarToggle");
 const closeSidebar = document.getElementById("closeSidebar");
 
 sidebarToggle.addEventListener("click", () => {
-    sidebar.style.right = "0"; 
+  sidebar.style.right = "0";
 });
 
 closeSidebar.addEventListener("click", () => {
-    sidebar.style.right = "-300px"; 
+  sidebar.style.right = "-300px";
 });
