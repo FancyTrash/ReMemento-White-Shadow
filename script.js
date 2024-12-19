b6554ea6e850fd26e61fdcb91e0f8f8487e4753f
FancyTrash <fancytrash786585@gmail.com>
2024-12-18 23:33:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const searchButton = document.getElementById("searchButton");
+const searchBox = document.getElementById("searchBox");
+const closeButton = document.getElementById("closeButton");
+
+// Search box toggle
+searchButton.addEventListener("click", () => {
+    searchBox.style.display = "flex";
+    searchButton.style.display = "none";
+});
+
+closeButton.addEventListener("click", () => {
+    searchBox.style.display = "none";
+    searchButton.style.display = "inline-block";
+});
+
+// Sidebar toggle
+const sidebar = document.getElementById("sidebar");
+const sidebarToggle = document.getElementById("sidebarToggle");
+const closeSidebar = document.getElementById("closeSidebar");
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.style.right = "0"; 
+});
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.style.right = "-300px"; 
+});
+
+// Scroll Arrow Logic
 const scrollArrow = document.getElementById("scrollArrow");
 const mainContent = document.querySelector(".main-content");
 
